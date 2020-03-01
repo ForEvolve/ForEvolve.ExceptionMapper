c3ebf1baf5532aaab7e3eaa037896cbf3d52fa63
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-03-01 01:37:16 -0500

    Move DI stuff into folder

---
+++ src/ForEvolve.ExceptionFilters.Abstraction/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ForEvolve.ExceptionFilters;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExceptionFiltersExtensions
+    {
+        public static IServiceCollection AddExceptionMapping(this IServiceCollection services, Action<IExceptionMappingBuilder> exceptionMappingBuilder = null)
+        {
+            services.AddLogging();
+            exceptionMappingBuilder?.Invoke(new ServiceCollectionWrapper(services));
+            services.TryAddSingleton<IExceptionHandlingManager, ExceptionHandlingManager>();
+            return services;
+        }
+    }
+}
