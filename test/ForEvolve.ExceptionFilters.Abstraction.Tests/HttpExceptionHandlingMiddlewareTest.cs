ee229cfdc497c9bc10d28b970a32e9df49fcd979
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-03-01 00:07:27 -0500

    Use mocks instead of implementations

--- test/ForEvolve.ExceptionFilters.Abstraction.Tests/HttpExceptionHandlingMiddlewareTest.cs
+++ test/ForEvolve.ExceptionFilters.Abstraction.Tests/HttpExceptionHandlingMiddlewareTest.cs
@@ -30,24 +30,36 @@ namespace ForEvolve.ExceptionFilters
         [Fact]
         public async Task Should_not_call_next_when_exception_is_handled()
         {
-            var result = new ExceptionHandledResult(new Exception());
+            // Arrange
+            var resultMock = new Mock<IExceptionHandlingResult>();
+            resultMock.Setup(x => x.ExceptionHandled).Returns(true);
             _exceptionHandlingManagerMock
                 .Setup(x => x.HandleAsync(It.IsAny<HttpContext>()))
-                .ReturnsAsync(result)
+                .ReturnsAsync(resultMock.Object)
             ;
+
+            // Act
             await sut.InvokeAsync(HttpContext);
+
+            // Assert
             Assert.False(_nextWasCalled);
         }
 
         [Fact]
         public async Task Should_call_next_when_exception_is_not_handled()
         {
-            var result = new ExceptionNotHandledResult(new Exception());
+            // Arrange
+            var resultMock = new Mock<IExceptionHandlingResult>();
+            resultMock.Setup(x => x.ExceptionHandled).Returns(false);
             _exceptionHandlingManagerMock
                 .Setup(x => x.HandleAsync(It.IsAny<HttpContext>()))
-                .ReturnsAsync(result)
+                .ReturnsAsync(resultMock.Object)
             ;
+
+            // Act
             await sut.InvokeAsync(HttpContext);
+
+            // Assert
             Assert.True(_nextWasCalled);
         }
 
