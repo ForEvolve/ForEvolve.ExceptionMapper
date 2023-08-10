d5e9248250e735c11d061d97c2fcb9a6a0ae8a1e
Carl-Hugo Marcotte <carlhugo@gmail.com>
2023-08-10 00:57:16 -0400

    Document base exceptions

--- src/ForEvolve.ExceptionMapper/CommonExceptions/ClientErrorException.cs
+++ src/ForEvolve.ExceptionMapper/CommonExceptions/ClientErrorException.cs
@@ -1,5 +1,9 @@
 ﻿namespace ForEvolve.ExceptionMapper;
 
+/// <summary>
+/// Client error responses (400 – 499)
+/// <br /><br />See also <seealso cref="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses"/>
+/// </summary>
 public abstract class ClientErrorException : ForEvolveException
 {
     public ClientErrorException()
--- src/ForEvolve.ExceptionMapper/CommonExceptions/ServerErrorException.cs
+++ src/ForEvolve.ExceptionMapper/CommonExceptions/ServerErrorException.cs
@@ -1,5 +1,9 @@
 ﻿namespace ForEvolve.ExceptionMapper;
 
+/// <summary>
+/// Server error responses (500 – 599)
+/// <br /><br />See also <seealso cref="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses"/>
+/// </summary>
 public abstract class ServerErrorException : ForEvolveException
 {
     public ServerErrorException()
